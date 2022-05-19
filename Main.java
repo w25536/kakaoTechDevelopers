f125092f458ac0d1841597494a92226530769984
Jungho Suh <w25536@gmail.com>
2022-05-19 16:38:50 +0900

    Created Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 public class Main {
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+
+
+        System.out.println(Arrays.toString(report));
+        return answer;
+    }
+
+
     public static void main(String[] args) {
+
+        solution(new String[]{"muzi", "frodo", "apeach", "neo"},
+                new String[]{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}, 2);
+
 
     }
 }
